80e29e9b6220270dc2a7bcf6d2bdc7c9974a6b77
crofleen <48178880+crofleen@users.noreply.github.com>
2025-07-08 18:38:37 +0300

    Add files via upload

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+
+    return (
+        <div className={styles['footer']}>
+            Sinergy_practice © 2024
+        </div>
+    )
+}
+
+export default Footer
